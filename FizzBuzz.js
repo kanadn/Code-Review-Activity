ad13897a5586b7e75bcbf4d73716eb1140e979b8
Kanad Naleshwarkar <kanad.naleshwarkar@gmail.com>
2023-02-23 14:17:43 -0500

    Added FizzBuzz.js base code

---
+++ FizzBuzz.js
@@ -0,0 +1,23 @@
+let i;
+    for (i=1; i<=100; i++)
+    {         
+        // number divisible by 3? print 'Fizz'
+        // in place of the number
+        else if ((i%3) == 0)
+            document.write("Fizz" + " ");               
+         
+        // number divisible by 5, print 'Buzz'
+        // in place of the number
+        else if ((i%5) == 0)                   
+            document.write("Buzz" + " "); 
+
+
+        // number divisible by 3 and 5 will
+        // always be divisible by 15, print
+        // 'FizzBuzz' in place of the number
+        if (i%15 == 0)   
+            document.write("FizzBuzz" + " ");              
+     
+        else // print the number       
+            document.write(i + " ");               
+    }
