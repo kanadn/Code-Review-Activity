38765789c5d2dd5b51d670f85fe0bede355f78a8
Chris Brown <dcbrown06@gmail.com>
2023-02-23 18:01:36 -0500

    String index error?

--- IsPalindrome.py
+++ IsPalindrome.py
@@ -1,6 +1,6 @@
 def is_palindrome(word):
     reverse_word = ""
-    for i in range(len(word), 0, -1):
+    for i in range(len(word) - 1, 0, -1):
         reverse_word += word[i]
     return word == reverse_word
 
